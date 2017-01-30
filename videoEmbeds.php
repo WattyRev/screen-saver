c4f59a753e277b4899a45781dc8dc681a935941b
Spencer Watson <swatson@bittitan.com>
2017-01-30 09:46:06 -0800

    Starting to add items to the embeds list

--- videoEmbeds.php
+++ videoEmbeds.php
@@ -5,5 +5,13 @@
     {
         "embed": "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/rnI5iwsyM9U?autoplay=true\" frameborder=\"0\" allowfullscreen></iframe>",
         "duration": 239
+    },
+    {
+        "embed": "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/2ZLrs9gX73M?autoplay=true\" frameborder=\"0\" allowfullscreen></iframe>",
+        "duration": 273
+    },
+    {
+        "embed": "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/P-8v-_kaZ38?autoplay-true\" frameborder=\"0\" allowfullscreen></iframe>",
+        "duration": "29"
     }
 ]
